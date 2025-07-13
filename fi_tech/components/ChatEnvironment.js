eb507cd3b2e0a71a180cf53497238f55d1c622aa
AndersonBlanco <andersonblanco913@gmail.com>
2025-07-13 05:55:35 -0400

    chat

--- fi_tech/components/ChatEnvironment.js
+++ fi_tech/components/ChatEnvironment.js
@@ -12,7 +12,7 @@ import { GiftedChatContext, GiftedAvatar, GiftedChat, InputToolbar, Avatar, util
 import Logo from "../assets/fiTech_logo.png";
 import { Icon } from "react-native-elements";
 import { useSharedValue } from "react-native-worklets-core";
-//import { OpenAI } from "openai/client.js";
+import { OpenAI } from "openai/client.js";
 
 //import SideNav from "../components/sideNav";
 //gluetsack-ui for dynamic alert chat prmopt pop ups offer after button click 
@@ -20,13 +20,13 @@ import { useSharedValue } from "react-native-worklets-core";
 //Tamagui 
 //react native windUI
 //react native ui kitten - for Date Picker and for spinner loading icon maybe 
-let a; 
-/*
+
+//open-ai api-key: 
 const openAI = new OpenAI({
-    apiKey: a,
-    dangerouslyAllowBrowser: false
+    apiKey: process.env.GPT_KEY,
+    dangerouslyAllowBrowser: true
 });
-*/
+
 export default function ChatEnvironment({viewStyle, isKeyboardInternallyHandled}){
  const [msg, setMsg] = useState("");
 const create_gpt_resposne = async(txt) =>{
